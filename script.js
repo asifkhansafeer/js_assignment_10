5c3b4f2da612810fe41d840c278215603acc372e
asifkhansafeer <asifkhantfm555@gmail.com>
2024-07-07 19:35:04 +0500

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+/* smallest value of the array
+
+ array = [1,3,55,44,32]
+var smallestvalue = array[0]
+for (var i = 0; i < array.length; i++){
+    if(array[i] < smallestvalue){
+        smallestvalue = array[i];
+    }
+}
+console.log(`${smallestvalue}`)
+*/
